82bf8dce252750c022664ca63602a160571c2186
David Gong <90584914+davidgonghs@users.noreply.github.com>
2022-04-15 12:33:51 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cge3padjnblm.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "davidgong-employee"
+customregion = "us-east-1"
